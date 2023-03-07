90ed1b6eb9705934dd17af2f4824496d8ff6d05b
Vishal Rathod <94773110+Vishalrathod8788@users.noreply.github.com>
2023-03-07 22:50:04 +0530

    Add...

--- ATM/src/ATM/ATM.java
+++ ATM/src/ATM/ATM.java
@@ -23,52 +23,20 @@ public class ATM
 			System.out.print("\n");
 		}
 	}
+	void deposite()
+	{
+		System.out.println("Enter Your Deposite : ");
+		deposite = sc.nextInt();
+		balance = balance + deposite;
+		System.out.print("Your Money Succsessfully Deposite : " + deposite);
+		System.out.print("\n");
+	}
 	
 	
 	public static void main(String[] args) 
 	{
 		ATM A = new ATM();
 		
-		
-		Scanner sc = new Scanner(System.in);
-		
-		while(true)
-		{
-			System.out.print("\n");
-			System.out.println("Choise 1 For Withdraw");
-			System.out.println("Choise 2 For Deposite");
-			System.out.println("Choise 3 For Balance");
-			System.out.println("Choise 4 For Exit");
-			System.out.println("Enter Any One Number :-");
-			
-			int Choise = sc.nextInt();
-			
-			switch (Choise)
-			{
-				case 1: 
-				{
-					A.withdraw();
-					break;
-				}
-				case 2:
-				{
-					A.deposite();
-					break;
-				}
-				case 3:
-				{
-					A.balance();
-					break;
-				}
-				case 4:
-				{
-					A.exit();
-					break;
-				}
-			default:
-				System.out.println("Unexpected Value: ");
-			}			
-		}
 	}
 	
 }
